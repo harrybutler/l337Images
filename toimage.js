d18d53fcdeee847889e92318e5c686dbf7a21aae
harrybutler <harryjamesbutler0@gmail.com>
2014-08-21 01:08:15 -0700

    Update toimage.js

--- toimage.js
+++ toimage.js
@@ -1,3 +1,27 @@
+var handleFileSelect = function(evt) {
+    var files = evt.target.files;
+    var file = files[0];
+
+    if (files && file) {
+        var reader = new FileReader();
+
+        reader.onload = function(readerEvt) {
+            var binaryString = readerEvt.target.result;
+            document.getElementById("base64textarea").value = btoa(binaryString);
+        };
+
+        reader.readAsBinaryString(file);
+        var base64 = reader.readAsBinaryString(file);
+        var chars = base64.split('');
+        var cWidth = base64.length;
+    }
+};
+
+if (window.File && window.FileReader && window.FileList && window.Blob) {
+    document.getElementById('filePicker').addEventListener('change', handleFileSelect, false);
+} else {
+    alert('The File APIs are not fully supported in this browser.');
+}
 function setPixel(imageData, x, y, r, g, b, a) {
     index = (x + y * imageData.width) * 4;
     imageData.data[index+0] = r;
@@ -10,14 +34,14 @@ element = document.getElementById("canvas1");
 c = element.getContext("2d");
 
 // read the width and height of the canvas
-width = element.width;
-height = element.height;
+width = cWidth;
+height = "1";
 
 // create a new pixel array
 imageData = c.createImageData(width, height);
 
 // draw random dots
-for (i = 0; i < 10000; i++) {
+for (i = 0; i < cWidth; i++) {
     x = Math.random() * width | 0; // |0 to truncate to Int32
     y = Math.random() * height | 0;
     r = Math.random() * 256 | 0;
