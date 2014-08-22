9072539608e04d2cbc7ca89e4e270f8e9822ea63
harrybutler <harryjamesbutler0@gmail.com>
2014-08-22 12:51:32 +1000

    Update toimagefinal.js

--- toimagefinal.js
+++ toimagefinal.js
@@ -1,9 +1,23 @@
+var num;
+var str = document.getElementById('base64textarea').value;
+function base64_to_base10(str) {
+    var order = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_-";
+    var base = order.length;
+    var num = 0, r;
+    while (str.length) {
+        r = order.indexOf(str.charAt(0));
+        str = str.substr(1);
+        num *= base;
+        num += r;
+    }
+    return num;
+}
 document.body.innerHTML += '<canvas id="canvas1"></canvas>';
 document.getElementById('someBox').innerHTML = '<canvas id="canvas1"></canvas>';
 var canvas = document.getElementById('canvas1');
 canvas.width = 100;
 canvas.height = 100;
-var pixels = canvas.width * canvas.height
+var pixels = canvas.width * canvas.height;
 function setPixel(imageData, x, y, r, g, b, a) {
 index = (x + y * imageData.width) * 4;
 imageData.data[index+0] = r;
