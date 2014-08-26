e68088fff53524e5a87ff76175c27fc66c0ef8aa
harrybutler <harryjamesbutler0@gmail.com>
2014-08-26 11:44:05 +1000

    Update baseconvert.php

--- baseconvert.php
+++ baseconvert.php
@@ -4,11 +4,11 @@ $base10 = base_convert($base64, 64, 10);
 ?>
 <html>
 <head>
-<script src="toimagefinal.js"></script>
+<script src="/toimagefinal.js"></script>
 </head>
 <body>
 <h1>Image</h1>
-    <textarea id="base64textarea" placeholder="Base64 will appear here" cols="50" rows="15"></textarea><br />
+    <textarea id="base64textarea" placeholder="Base64 will appear here" cols="50" rows="15"><?php echo $base10; ?></textarea><br />
 <button onclick="base64_to_base10()">Click me</button>
 <div id="someBox"></div>
 </body>
